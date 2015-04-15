72ae81eadfb6552968ec2895db30c0ecd732c37b
Tiago Royer <royertiago@gmail.com>
2015-04-14 22:53:29 -0300

    Fix bugs in Zero2.

--- Zero2.cpp
+++ Zero2.cpp
@@ -1,26 +1,19 @@
 #include "Zero2.h"
 #include "core/util.h"
 namespace wagner {
-	Zero2::Zero2(std::string name):_name(name)
-	{
-
-	}
-
-	Zero2::~Zero2()
-	{
-
-    }
+	Zero2::Zero2( std::string name ) :
+        _name(name)
+	{}
 	int Zero2::hand() {
 		return 0;
 	}
 	int Zero2::guess () {
-	    core::chopstick_count()/2;
-
-
+	    int guess = core::chopstick_count()/2;
+        while( !core::valid_guess(guess) )
+            guess = (guess + 1) % core::chopstick_count();
+        return guess;
 	}
     std::string Zero2::name() const{
-
+        return "zero2";
     }
-
 }
-
--- Zero2.h
+++ Zero2.h
@@ -6,21 +6,14 @@ namespace wagner{
 
     class Zero2 : public Player {
         public:
-
-            Zero2(std::string name="Zero2");
-            virtual ~Zero2();
+            Zero2(std::string name = "Zero2");
 
             std::string name() const override;
-
             int hand() override;
-
             int guess() override;
-
             void end_round(){}
             void begin_game(){}
             void end_game(){}
-
-
         protected:
            std::string _name;
         private:
